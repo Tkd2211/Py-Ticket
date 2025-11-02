b6163a1dbc00c3760fc837278ee1a36cec4f9b0c
Tejas Dubey <123975589+Tkd2211@users.noreply.github.com>
2025-11-03 01:35:32 +0530

    Refactor get_input function with error prompts

--- Project_CS50/my_utilities/my_functions.py
+++ Project_CS50/my_utilities/my_functions.py
@@ -1,38 +1,49 @@
-#GET USER INPUT
-def get_input(prompt="input: ", type="str", required=False, give_parameters=False):
+# GET USER INPUT
+def get_input(prompt="input: ", input_type="str", required=False, give_parameters=False, error_prompt=None):
     if give_parameters:
-        print("get_input(prompt=\"input: \", type=\"str\", required=False)")
+        print('get_input(prompt="input: ", type="str", required=False)')
         return None
-    
+
     while True:
-        if type == "str":
+        if input_type == "str":
             value = input(prompt).strip()
-        elif type == "int":
+        elif input_type == "int":
             try:
                 value = int(input(prompt))
             except ValueError:
-                print("Not an integer value")
+                if error_prompt==None:
+                    print("Not an integer value")
+                else:
+                    print(error_prompt)
                 continue
-        elif type == "float":
+        elif input_type == "float":
             try:
                 value = float(input(prompt))
             except ValueError:
-                print("Not a floating point value")
+                if error_prompt==None:
+                    print("Not a floating point value")
+                else:
+                    print(error_prompt)
                 continue
         else:
-            raise ValueError(f"Invalid type '{type}' passed to get_input(). Valid types: 'str', 'int', 'float'.")
+            raise ValueError(
+                f"Invalid type '{input_type}' passed to get_input(). Valid types: 'str', 'int', 'float'."
+            )
 
-        if required and (value == "" or value is None):
+        if required and (value== "" or value is None):
             print("This is a required field")
             continue
 
         return value
 
-#Example_of_use
+
+# Example_of_use
+
 
 def use_example():
-    num=get_input("Whats the number yo? ",type="int",required=True)
+    num = get_input("Whats the number? ", input_type="int", required=True, error_prompt="Bad")
     print("The number you typed is:", num)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     use_example()
