4cab713089a5a894f55f47c79778b0c4c81ab0e8
Tejas Dubey <123975589+Tkd2211@users.noreply.github.com>
2025-11-07 15:54:23 +0530

    Refactor Train class and booking system
    > Refactored train class to handle the logic booking logic cleanly.
    > Fixed ticket output to only include details for only a single group of passengers instead of storing data for all passengers.
    > Refactored assign_seat_number() to handle edge cases like all seats getting booked.
    > Added error handling for create_ticket() function.

--- Project_CS50/project_CS50.py
+++ Project_CS50/project_CS50.py
@@ -3,9 +3,12 @@ from my_utilities.my_functions import get_input
 import json
 from pathlib import Path
 import random 
-import sys
 
 registered_users = ["User_1", "User_2"]
+
+LOWER = "Lower-seat"
+MIDDLE = "Middle-seat"
+UPPER = "Upper-seat"
 
 def get_json_data_path(file):
     BASE_DIR = Path(__file__).resolve().parent
@@ -19,66 +22,55 @@ def get_json_data_path(file):
 # Train class to handle seat bookings
 class Train:
     def __init__(self):
-        self._train_seats_data = get_json_data_path(file="train_seats_count")
-        if self._train_seats_data.exists():
-            with open(self._train_seats_data,"r") as data:
-                self.seats_data = json.load(data) 
-        else:
-            with open(self._train_seats_data,"w") as data:
-                json.dump({"seats": {"upper_seats": 27, "lower_seats": 27, "middle_seats": 18 }}, data, indent=4)
-            with open(self._train_seats_data,"r") as data:
-                self.seats_data = json.load(data)
+        self._train_seats_data = get_json_data_path("train_seats_count")
+        if not self._train_seats_data.exists():
+            with open(self._train_seats_data, "w") as file:
+                json.dump({"seats": {"upper_seats": 27, "lower_seats": 27, "middle_seats": 18}}, file, indent=4)
+
+        with open(self._train_seats_data, "r") as data:
+            self.seats_data = json.load(data)
+
+    def save(self):
+        with open(self._train_seats_data, "w") as file:
+            json.dump(self.seats_data, file, indent=4)
 
     def book_lower(self):
         if self.seats_data["seats"]["lower_seats"] > 0:
             self.seats_data["seats"]["lower_seats"] -= 1
-            with open(self._train_seats_data,"w") as file:
-                json.dump(self.seats_data, file, indent=4)
+            self.save()
             print("Lower seat booked successfully!")
+            return LOWER
         else:
-            status = self.book_middle()
-            return status
-
-    def book_upper(self):
-        if self.seats_data["seats"]["upper_seats"] > 0:
-            self.seats_data["seats"]["upper_seats"] -= 1
-            with open(self._train_seats_data,"w") as file:
-                json.dump(self.seats_data, file, indent=4)
-            print("Upper seat booked successfully!")
-        elif self.seats_data["seats"]["upper_seats"] + self.seats_data["seats"]["middle_seats"] + self.seats_data["seats"]["lower_seats"] > 0:
-            if self.seats_data["seats"]["middle_seats"] > 0:
-                self.book_middle()
-            elif self.seats_data["seats"]["lower_seats"]:
-                self.book_lower()
-        else:
-            return "Full"
+            return self.book_middle()
 
     def book_middle(self):
         if self.seats_data["seats"]["middle_seats"] > 0:
             self.seats_data["seats"]["middle_seats"] -= 1
-            with open(self._train_seats_data,"w") as file:
-                json.dump(self.seats_data, file, indent=4)
+            self.save()
             print("Middle seat booked successfully!")
+            return MIDDLE
         else:
-            status = self.book_upper()
-            return status
-        
+            return self.book_upper()
 
-#seat number data structure
-def assign_seat_number(seat_type):                        #Handle IndexError in case all seats in a seat type getting booked (empty list).
-    train_seat_number = get_json_data_path(file="train_seat_number")
-    seat_number_array = {}
-    with open(train_seat_number,"r") as seat_number_data:                    
-        seat_number_array = json.load(seat_number_data)
-        if seat_type == "Lower-seat":                     #Remove the conditional block from being indented inside file I/O
-                seat_number = seat_number_array["seat_number"]["lower_seat_number"].pop(0)
-        elif seat_type == "Middle-seat":
-            seat_number = seat_number_array["seat_number"]["middle_seat_number"].pop(0)
+    def book_upper(self):
+        if self.seats_data["seats"]["upper_seats"] > 0:
+            self.seats_data["seats"]["upper_seats"] -= 1
+            self.save()
+            print("Upper seat booked successfully!")
+            return UPPER
+        elif (
+            self.seats_data["seats"]["upper_seats"]
+            + self.seats_data["seats"]["middle_seats"]
+            + self.seats_data["seats"]["lower_seats"]
+            == 0
+        ):
+            print("All seats are booked! No seats available.")
+            return None
         else:
-            seat_number = seat_number_array["seat_number"]["upper_seat_number"].pop(0)
-    with open(train_seat_number,"w") as seat_number_file:
-        json.dump(seat_number_array, seat_number_file, indent=4)
-    return seat_number
+            if self.seats_data["seats"]["middle_seats"] > 0:
+                return self.book_middle()
+            elif self.seats_data["seats"]["lower_seats"] > 0:
+                return self.book_lower()
 
 
 # Main function to run the ticket booking system
@@ -90,6 +82,24 @@ def main():
     book_ticket(username, passenger_count)
 
 
+#seat number data structure
+def assign_seat_number(seat_type):
+    train_seat_number = get_json_data_path(file="train_seat_number")
+    with open(train_seat_number,"r") as seat_number_data:
+        seat_number_array = json.load(seat_number_data)
+    try:    
+        if seat_type == "Lower-seat":
+                seat_number = seat_number_array["seat_number"]["lower_seat_number"].pop(0)
+        elif seat_type == "Middle-seat":
+            seat_number = seat_number_array["seat_number"]["middle_seat_number"].pop(0)
+        else:
+            seat_number = seat_number_array["seat_number"]["upper_seat_number"].pop(0)
+    except IndexError:
+        return None
+
+    with open(train_seat_number,"w") as seat_number_file:
+        json.dump(seat_number_array, seat_number_file, indent=4)
+    return seat_number
 
 # Function to verify registered users
 def get_verified_user():
@@ -103,7 +113,7 @@ def get_verified_user():
 
 # Function to get booking details from the user
 def get_booking_details(i):  
-    print(f"ENTER YOUR BOOKING DETAILS HERE FOR PASSENGER {i+1}:")
+    print(f"\nENTER YOUR BOOKING DETAILS HERE FOR PASSENGER {i+1}:")
     name = get_input(prompt="Name: ", required=True)
     age = get_input(prompt="Age: ", required=True, input_type="int", error_prompt="Not a valid age!")
     return (name, age)
@@ -111,54 +121,43 @@ def get_booking_details(i):
 # AGE-FILTER
 # ADD ALGORITHM TO BOOK SEAT TYPE BASED ON AGE FILTERING
 def book_ticket(username, passenger_count):
+    train = Train()
+    with open("ticket.txt","w") as file:
+        pass
     for i in range(passenger_count):
         name, age = get_booking_details(i)
-        try:                                                         
-            train = Train()                        #Move this instantiation out of the loop 
-        except Exception:
-            sys.exit("Error occured!")
         if age > 60:
-            status = train.book_lower()
-            if status == "Full":
-                print("All seats are booked! No seats available.")
-                return None
-            else: 
-                seat_type = "Lower-seat"    
+            seat_type = train.book_lower()
         elif 30 <= age <= 60:
-            status = train.book_middle()
-            if status == "Full":
-                print("All seats are booked! No seats available.")
-                return None
-            else: 
-                seat_type = "Middle-seat"
+            seat_type = train.book_middle()
         else:
-            status = train.book_upper()
-            if status == "Full":
-                print("All seats are booked! No seats available.")
-                return None
-            else:
-                seat_type = "Upper-seat" 
+            seat_type = train.book_upper()
+        if not seat_type:
+            return None
         create_ticket(username, name, age, seat_type)
 
 # ID Genrator
 def id_genrator(seat_type):
-    if seat_type == "Lower-seat":
-        seat_code = "L"
-    elif seat_type == "Middle-seat":
-        seat_code = "M"
+    if seat_type != None:
+        seat_code = seat_type[0]
     else:
-        seat_code = "U"
+        return None
     return f"{seat_code}-{random.randint(0,9)}{random.choice('ABCDEFGHIJKLMNPQRSTUVWXYZ')}{random.randint(0,9)}{random.choice('ABCDEFGHIJKLMNPQRSTUVWXYZ')}"
 
 
 # Function to create and save the ticket details
-def create_ticket(username, name, age, seat_type):   #Reset ticket.txt after creating ticket for multiple passengers.
-    with open("ticket1.txt", "a") as file:           #call id_generator() and assigin_seat_number() outside file I/O, to handle errors
+def create_ticket(username, name, age, seat_type):  
+    if seat_type != None:
+        seat_number = assign_seat_number(seat_type)
+    else:
+        return None
+    with open("ticket.txt", "a") as file:   
         file.write(
-            f"Ticket under : {username}\n Ticket Id: {id_genrator(seat_type)}\n Name: {name}\n Age: {age}\n Seat:{assign_seat_number(seat_type)} {seat_type}\n"
+            f"Ticket under : {username}\n Ticket Id: {id_genrator(seat_type)}\n Name: {name}\n Age: {age}\n Seat:{seat_number} {seat_type}\n\n"
         )
 
 #CREATE A BOOKING CHART TO DISPLAY ALL PASSENGERS IN THE TRAIN (JSON FILE)
+
 
 if __name__ == "__main__":
     main()
