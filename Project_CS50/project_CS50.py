6109eaa1099858bcc15463fe1f562884e332fb66
Tejas Dubey <123975589+Tkd2211@users.noreply.github.com>
2025-11-12 20:03:39 +0530

    Implement ticket cancellation and user registration
    Added ticket cancellation functionality and updated user registration.

--- Project_CS50/project_CS50.py
+++ Project_CS50/project_CS50.py
@@ -2,13 +2,14 @@ import pyfiglet
 from my_utilities.my_functions import get_input
 import json
 from pathlib import Path
-import random 
+import random
 
-registered_users = ["User_1", "User_2"]
+registered_users = ["User_1", "User_2","User_3"]
 
 LOWER = "Lower-seat"
 MIDDLE = "Middle-seat"
 UPPER = "Upper-seat"
+
 
 def get_json_data_path(file):
     BASE_DIR = Path(__file__).resolve().parent
@@ -19,15 +20,28 @@ def get_json_data_path(file):
     elif file == "booking_chart":
         return BASE_DIR / "data" / "booking_chart.json"
     else:
-        raise ValueError(f"Invalid file name: {file}. Expected 'train_seats_count' or 'train_seat_number'.")
+        raise ValueError(
+            f"Invalid file key: {file}. Expected 'train_seats_count' or 'train_seat_number'."
+        )
+
 
 def load_json(file):
     BASE_DIR = Path(__file__).resolve().parent
     if file == "train_seats_count":
         file_path = BASE_DIR / "data" / "train_seats_count.json"
         if not file_path.exists():
-            with open(file_path,"w") as file:
-                json.dump({"seats": {"upper_seats": 27, "lower_seats": 27, "middle_seats": 18 }}, file, indent=4)
+            with open(file_path, "w") as file:
+                json.dump(
+                    {
+                        "seats": {
+                            UPPER: 27,
+                            LOWER: 27,
+                            MIDDLE: 18,
+                        }
+                    },
+                    file,
+                    indent=4,
+                )
             with open(file_path) as data:
                 return json.load(data)
         else:
@@ -37,10 +51,10 @@ def load_json(file):
     elif file == "train_seat_number":
         file_path = BASE_DIR / "data" / "train_seat_number.json"
         if not file_path.exists():
-            with open(file_path,"w") as file:
-                json.dump({'seat_number': {'upper_seat_number': [3, 6, 8, 11, 14, 16, 19, 22, 24, 27, 30, 32, 35, 38, 40, 43, 46, 51, 56, 59, 64, 67, 70, 72], 
-                                    'lower_seat_number': [1, 4, 7, 9, 12, 15, 17, 20, 23, 25, 28, 31, 33, 36, 39, 41, 44, 47, 52, 55, 57, 60, 63, 65, 68, 71], 
-                                    'middle_seat_number': [2, 5, 10, 13, 18, 21, 26, 29, 34, 37, 42, 45, 50, 53, 58, 61, 66, 69]}}, file, indent=4)
+            with open(file_path, "w") as file:
+                json.dump({'seat_number': {UPPER: [3, 6, 8, 11, 14, 16, 19, 22, 24, 27, 30, 32, 35, 38, 40, 43, 46, 51, 56, 59, 64, 67, 70, 72],
+                                    LOWER: [1, 4, 7, 9, 12, 15, 17, 20, 23, 25, 28, 31, 33, 36, 39, 41, 44, 47, 52, 55, 57, 60, 63, 65, 68, 71], 
+                                    MIDDLE: [2, 5, 10, 13, 18, 21, 26, 29, 34, 37, 42, 45, 50, 53, 58, 61, 66, 69]}}, file, indent=4)
             with open(file_path) as data:
                 return json.load(data)
         else:
@@ -49,15 +63,18 @@ def load_json(file):
     elif file == "booking_chart":
         file_path = BASE_DIR / "data" / "booking_chart.json"
         if not file_path.exists():
-            with open(file_path,"w") as file:
+            with open(file_path, "w") as file:
                 json.dump({"bookings": {}}, file, indent=4)
             with open(file_path) as data:
-                    return json.load(data)
+                return json.load(data)
         else:
             with open(file_path) as data:
                 return json.load(data)
     else:
-        raise ValueError(f"Invalid file name: {file}. Expected 'train_seats_count', 'train_seat_number' or 'booking_chart'.")
+        raise ValueError(
+            f"Invalid file name: {file}. Expected 'train_seats_count', 'train_seat_number' or 'booking_chart'."
+        )
+
 
 # Train class to handle seat bookings
 class Train:
@@ -70,8 +87,8 @@ class Train:
             json.dump(self.seats_data, file, indent=4)
 
     def book_lower(self):
-        if self.seats_data["seats"]["lower_seats"] > 0:
-            self.seats_data["seats"]["lower_seats"] -= 1
+        if self.seats_data["seats"][LOWER] > 0:
+            self.seats_data["seats"][LOWER] -= 1
             self.save()
             print("Lower seat booked successfully!")
             return LOWER
@@ -79,8 +96,8 @@ class Train:
             return self.book_middle()
 
     def book_middle(self):
-        if self.seats_data["seats"]["middle_seats"] > 0:
-            self.seats_data["seats"]["middle_seats"] -= 1
+        if self.seats_data["seats"][MIDDLE] > 0:
+            self.seats_data["seats"][MIDDLE] -= 1
             self.save()
             print("Middle seat booked successfully!")
             return MIDDLE
@@ -88,59 +105,68 @@ class Train:
             return self.book_upper()
 
     def book_upper(self):
-        if self.seats_data["seats"]["upper_seats"] > 0:
-            self.seats_data["seats"]["upper_seats"] -= 1
+        if self.seats_data["seats"][UPPER] > 0:
+            self.seats_data["seats"][UPPER] -= 1
             self.save()
             print("Upper seat booked successfully!")
             return UPPER
         elif (
-            self.seats_data["seats"]["upper_seats"]
-            + self.seats_data["seats"]["middle_seats"]
-            + self.seats_data["seats"]["lower_seats"]
+            self.seats_data["seats"][UPPER]
+            + self.seats_data["seats"][MIDDLE]
+            + self.seats_data["seats"][LOWER]
             == 0
         ):
             print("All seats are booked! No seats available.")
             return None
         else:
-            if self.seats_data["seats"]["middle_seats"] > 0:
+            if self.seats_data["seats"][MIDDLE] > 0:
                 return self.book_middle()
-            elif self.seats_data["seats"]["lower_seats"] > 0:
+            elif self.seats_data["seats"][LOWER] > 0:
                 return self.book_lower()
 
 
 # Main function to run the ticket booking system
 def main():
-    f = pyfiglet.Figlet(font="slant")  
+    f = pyfiglet.Figlet(font="slant")
     print(f.renderText("WELCOME TO PY-TICKET"))
-    book_or_cancel = get_input(prompt="Type 'Cancel' if you want to cancel a ticket or PRESS Enter to book a ticket: ",error_prompt="Not a valid input!")
-    if book_or_cancel.lower() == 'cancel':
+    book_or_cancel = get_input(
+        prompt="Type 'Cancel' if you want to cancel a ticket or PRESS Enter to book a ticket: ",
+        error_prompt="Not a valid input!",
+    )
+    if book_or_cancel.lower() == "cancel":
         cancel_ticket(get_input(prompt="Ticket_id: ", required=True))
-    elif book_or_cancel is (None or ''): 
+    elif book_or_cancel is (None or ""):
         username = get_verified_user()
-        passenger_count = get_input(prompt="How many passengers would you like to book seats for? ", input_type="int", error_prompt="Not a valid passenger amount!")
+        passenger_count = get_input(
+            prompt="How many passengers would you like to book seats for? ",
+            input_type="int",
+            error_prompt="Not a valid passenger amount!",
+        )
         book_ticket(username, passenger_count)
     else:
         print("Not a valid input!")
 
-#seat number data structure
+
+# seat number data structure
 def assign_seat_number(seat_type):
     seat_number_array = load_json("train_seat_number")
-    try:    
-        if seat_type == LOWER :
-                seat_number = seat_number_array["seat_number"]["lower_seat_number"].pop(0)
+    try:
+        if seat_type == LOWER:
+            seat_number = seat_number_array["seat_number"][LOWER].pop(0)
         elif seat_type == MIDDLE:
-            seat_number = seat_number_array["seat_number"]["middle_seat_number"].pop(0)
+            seat_number = seat_number_array["seat_number"][MIDDLE].pop(0)
         elif seat_type == UPPER:
-            seat_number = seat_number_array["seat_number"]["upper_seat_number"].pop(0)
+            seat_number = seat_number_array["seat_number"][UPPER].pop(0)
         else:
-            return None
+          return None
     except IndexError:
         return None
 
-    with open(get_json_data_path("train_seat_number"),"w") as seat_number_file:
+    with open(get_json_data_path("train_seat_number"), "w") as seat_number_file:
         json.dump(seat_number_array, seat_number_file, indent=4)
     return seat_number
-    
+
+
 # Function to verify registered users
 def get_verified_user():
     while True:
@@ -152,17 +178,19 @@ def get_verified_user():
 
 
 # Function to get booking details from the user
-def get_booking_details(i):  
+def get_booking_details(i):
     print(f"\nENTER YOUR BOOKING DETAILS HERE FOR PASSENGER {i+1}:")
     name = get_input(prompt="Name: ", required=True)
-    age = get_input(prompt="Age: ", required=True, input_type="int", error_prompt="Not a valid age!")
+    age = get_input(
+        prompt="Age: ", required=True, input_type="int", error_prompt="Not a valid age!"
+    )
     return (name, age)
 
-# AGE-FILTER
+
 # ADD ALGORITHM TO BOOK SEAT TYPE BASED ON AGE FILTERING
 def book_ticket(username, passenger_count):
     train = Train()
-    with open("ticket.txt","w") as file:
+    with open("ticket.txt", "w") as file:
         pass
     for i in range(passenger_count):
         name, age = get_booking_details(i)
@@ -176,6 +204,7 @@ def book_ticket(username, passenger_count):
             return None
         create_ticket(username, name, age, seat_type)
 
+
 # ID Genrator
 def id_genrator(seat_type):
     if seat_type != None:
@@ -186,34 +215,53 @@ def id_genrator(seat_type):
 
 
 # Function to create and save the ticket details
-def create_ticket(username, name, age, seat_type):  
+def create_ticket(username, name, age, seat_type):
     if seat_type != None:
         seat_number = assign_seat_number(seat_type)
         id = id_genrator(seat_type)
     else:
         return None
-    with open("ticket.txt", "a") as file:   
+    with open("ticket.txt", "a") as file:
         file.write(
             f"Ticket under : {username}\n Ticket Id: {id}\n Name: {name}\n Age: {age}\n Seat:{seat_number} {seat_type}\n\n"
         )
     add_to_booking_chart(username, id, name, age, seat_type, seat_number)
 
-#CREATE A BOOKING CHART TO DISPLAY ALL PASSENGERS IN THE TRAIN (JSON FILE)
+
+# CREATE A BOOKING CHART TO DISPLAY ALL PASSENGERS IN THE TRAIN (JSON FILE)
 def add_to_booking_chart(username, id, name, age, seat_type, seat_number):
     new_booking = {
         "username": username,
         "name": name,
         "age": age,
         "seat_number": seat_number,
-        "seat_type": seat_type
-        }
+        "seat_type": seat_type,
+    }
     ticket_details = load_json("booking_chart")
     ticket_details["bookings"].update({id: new_booking})
-    with open(get_json_data_path("booking_chart"), 'w') as file:
-        json.dump(ticket_details,file, indent = 4)
+    with open(get_json_data_path("booking_chart"), "w") as file:
+        json.dump(ticket_details, file, indent=4)
 
-#def cancel_ticket(cancellation_id):
+
+# TICKET CANCELLATION BASED ON UNIQUE TICKET ID.
+def cancel_ticket(cancel_id):
+    chart_data = load_json("booking_chart")
+    seat_count = load_json("train_seats_count")
+    seat_number = load_json("train_seat_number")
+    if cancel_id not in chart_data["bookings"]:
+        return f"Invalid ID:{cancel_id}! Failed to cancel the ticket"
+    cancelled_seat_type = chart_data["bookings"][cancel_id]["seat_type"]
+    cancelled_seat_number = chart_data["bookings"][cancel_id]["seat_number"]
+    seat_count["seats"][cancelled_seat_type] += 1
+    seat_number["seat_number"][cancelled_seat_type].insert(0, cancelled_seat_number)
+    del chart_data["bookings"][cancel_id]
+    with open(get_json_data_path("train_seats_count"), "w") as file:
+        json.dump(seat_count, file, indent=4)
+    with open(get_json_data_path("train_seat_number"),"w") as file:
+        json.dump(seat_number, file, indent=4)
+    with open(get_json_data_path("booking_chart"),"w") as file:
+        json.dump(chart_data, file, indent=4)
+    return f"Ticket with ID:{cancel_id} successfully cancelled"
 
 if __name__ == "__main__":
     main()
-
