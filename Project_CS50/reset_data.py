ef3672d255f34a5f1d53f3ca54299b786c459664
Tejas Dubey <123975589+Tkd2211@users.noreply.github.com>
2025-11-12 20:04:27 +0530

    Refactor reset_data.py to use constants for seats

--- Project_CS50/reset_data.py
+++ Project_CS50/reset_data.py
@@ -1,7 +1,7 @@
 import json
 import sys
 from pathlib import Path
-from project_CS50 import get_json_data_path
+from project_CS50 import get_json_data_path, LOWER, MIDDLE, UPPER
 
 def main():
     file_name = get_argument()
@@ -23,12 +23,12 @@ def reset(file_name):
     match file_name:
         case "train_seats_count":
             with open(reset_train_seats_count,"w") as file:
-                json.dump({"seats": {"upper_seats": 27, "lower_seats": 27, "middle_seats": 18 }}, file, indent=4)
+                json.dump({"seats": {UPPER: 27, LOWER: 27, MIDDLE: 18 }}, file, indent=4)
         case "train_seat_number":
             with open(reset_train_seat_number,"w") as file:
-                json.dump({'seat_number': {'upper_seat_number': [3, 6, 8, 11, 14, 16, 19, 22, 24, 27, 30, 32, 35, 38, 40, 43, 46, 51, 56, 59, 64, 67, 70, 72], 
-                                    'lower_seat_number': [1, 4, 7, 9, 12, 15, 17, 20, 23, 25, 28, 31, 33, 36, 39, 41, 44, 47, 52, 55, 57, 60, 63, 65, 68, 71], 
-                                    'middle_seat_number': [2, 5, 10, 13, 18, 21, 26, 29, 34, 37, 42, 45, 50, 53, 58, 61, 66, 69]}}, file, indent=4)
+                json.dump({'seat_number': {UPPER: [3, 6, 8, 11, 14, 16, 19, 22, 24, 27, 30, 32, 35, 38, 40, 43, 46, 51, 56, 59, 64, 67, 70, 72], 
+                                    LOWER: [1, 4, 7, 9, 12, 15, 17, 20, 23, 25, 28, 31, 33, 36, 39, 41, 44, 47, 52, 55, 57, 60, 63, 65, 68, 71], 
+                                    MIDDLE: [2, 5, 10, 13, 18, 21, 26, 29, 34, 37, 42, 45, 50, 53, 58, 61, 66, 69]}}, file, indent=4)
         case "booking_chart":
             with open(reset_booking_chart, "w") as file:
                 json.dump({"bookings": []}, file, indent=4)
@@ -36,11 +36,13 @@ def reset(file_name):
             with open(reset_booking_chart, "w") as file:
                 json.dump({"bookings": {}}, file, indent=4)
             with open(reset_train_seats_count,"w") as file:
-                json.dump({"seats": {"upper_seats": 27, "lower_seats": 27, "middle_seats": 18 }}, file, indent=4)
+                json.dump({"seats": {UPPER: 27, LOWER: 27, MIDDLE: 18 }}, file, indent=4)
             with open(reset_train_seat_number,"w") as file:
-                json.dump({'seat_number': {'upper_seat_number': [3, 6, 8, 11, 14, 16, 19, 22, 24, 27, 30, 32, 35, 38, 40, 43, 46, 51, 56, 59, 64, 67, 70, 72],
-                                    'lower_seat_number': [1, 4, 7, 9, 12, 15, 17, 20, 23, 25, 28, 31, 33, 36, 39, 41, 44, 47, 52, 55, 57, 60, 63, 65, 68, 71], 
-                                    'middle_seat_number': [2, 5, 10, 13, 18, 21, 26, 29, 34, 37, 42, 45, 50, 53, 58, 61, 66, 69]}}, file, indent=4)
+                json.dump({'seat_number': {UPPER: [3, 6, 8, 11, 14, 16, 19, 22, 24, 27, 30, 32, 35, 38, 40, 43, 46, 51, 56, 59, 64, 67, 70, 72],
+                                    LOWER: [1, 4, 7, 9, 12, 15, 17, 20, 23, 25, 28, 31, 33, 36, 39, 41, 44, 47, 52, 55, 57, 60, 63, 65, 68, 71], 
+                                    MIDDLE: [2, 5, 10, 13, 18, 21, 26, 29, 34, 37, 42, 45, 50, 53, 58, 61, 66, 69]}}, file, indent=4)
+            with open("ticket.txt", "w") as file:
+                pass       
         case _:
             sys.exit("Not a valid file name")
 
