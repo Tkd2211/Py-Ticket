2651138fa036ef1e2def5361f745485130bde1a7
Tejas Dubey <123975589+Tkd2211@users.noreply.github.com>
2025-11-25 16:19:31 +0530

    Enhance get_input with negative value handling
    Added support for negative value checks and prioritized error prompts in get_input function.

--- Project_CS50/my_utilities/my_functions.py
+++ Project_CS50/my_utilities/my_functions.py
@@ -5,12 +5,16 @@ def get_input(
     required=False,
     give_parameters=False,
     error_prompt=None,
+    negative_value_allowed=False,
 ):
+    # To make specifed error prompt a priority over generic error prompt
+    priority_error_prompt = error_prompt
+    
     if give_parameters:
         print('get_input(prompt="input: ", type="str", required=False)')
         return None
-
     while True:
+        ValueError_prompt = None
         if input_type == "str":
             try:
                 value = input(prompt).strip()
@@ -24,21 +28,35 @@ def get_input(
         elif input_type == "int":
             try:
                 value = int(input(prompt).strip())
+                if value < 0 and negative_value_allowed == False:
+                    ValueError_prompt = "This field does not take negative values"
+                    raise ValueError(ValueError_prompt)
             except ValueError:
+                error_prompt = priority_error_prompt
+                if ValueError_prompt and error_prompt == None:
+                    error_prompt = ValueError_prompt
                 if error_prompt == None:
                     print("Not an integer value")
                 else:
                     print(error_prompt)
+                    error_prompt = None
                 continue
         
         elif input_type == "float":
             try:
                 value = float(input(prompt).strip())
+                if value < 0 and negative_value_allowed == False:
+                    ValueError_prompt = "This field does not take negative values"
+                    raise ValueError(ValueError_prompt)
             except ValueError:
+                error_prompt = priority_error_prompt
+                if ValueError_prompt and error_prompt == None:
+                    error_prompt = ValueError_prompt
                 if error_prompt == None:
                     print("Not a floating point value")
                 else:
                     print(error_prompt)
+                    error_prompt = None
                 continue
         
         else:
@@ -54,8 +72,6 @@ def get_input(
 
 
 # Example_of_use
-
-
 def use_example():
     num = get_input(
         "Whats the number? ", input_type="int", required=True, error_prompt="Bad"
